517571f64974edcfd29bd5727c623e2baa4f90d7
Alexander Solan <alexander.solan@gmail.com>
2013-12-28 17:55:22 +0200

    Initial commit.

---
+++ Assets/Scripts/HexBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexBehaviour : MonoBehaviour {
+
+	public GameObject parentisation;
+
+	// Use this for initialization
+	void Start () {
+		parentisation = GameObject.Find ("HexFolder");
+		this.transform.parent = parentisation.transform; // Cleans up nicely. :P
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexGrid : MonoBehaviour {
+	public Transform spawnThis;   
+	
+	public int x = 5;
+	public int z = 5;
+
+	public int mapWidth = 25;
+	public int mapLength = 25;
+
+	public float radius = 0.5f;
+	public bool useAsInnerCircleRadius = true;
+	
+	private float offsetX, offsetZ;
+
+	public Transform [] tile; // tile types
+	
+	//Inn, road, grass, forest, dungeon
+
+	private int[][] mapMaking = 
+	{
+		new int[]{ 3, 3, 3, 3, 3, 3, 3 },
+		new int[]{ 3, 0, 2, 2, 2, 2, 3 },
+		new int[]{ 3, 2, 1, 1, 1, 2, 3 },
+		new int[]{ 3, 2, 2, 2, 2, 4, 3 },
+		new int[]{ 3, 3, 3, 3, 3, 3, 3 },
+		
+	};
+
+	void Start() {
+		float unitLength = ( useAsInnerCircleRadius )? (radius / (Mathf.Sqrt(3)/2)) : radius;
+		
+		offsetX = unitLength * Mathf.Sqrt(3);
+		offsetZ = unitLength * 1.5f;
+		
+
+		for( int i = 0; i < 5; i++ ) {
+			for( int j = 0; j < 7; j++ ) {
+				Vector3 hexpos = HexOffset( i, j );
+				Vector3 pos = new Vector3( hexpos.x, 0, hexpos.z);
+				Instantiate(tile[ mapMaking[i][j] ], pos, transform.rotation );
+			}
+		}
+	}
+	
+	Vector3 HexOffset( int x, int z ) {
+		Vector3 position = Vector3.zero;
+		
+		if( z % 2 == 0 ) {
+			position.x = x * offsetX;
+			position.z = z * offsetZ;
+		}
+		else {
+			position.x = ( x + 0.5f ) * offsetX;
+			position.z = z * offsetZ;
+		}
+		
+		return position;
+	}
+}
