6790ebddc6ddb95507f2b7872a5750f1ff9a586e
Alexander Solan <alexander.solan@gmail.com>
2013-12-28 21:05:44 +0200

    Tile generation complete.

--- Assets/Scripts/HexBehaviour.cs
+++ Assets/Scripts/HexBehaviour.cs
@@ -4,12 +4,21 @@ using System.Collections;
 public class HexBehaviour : MonoBehaviour {
 
 	public GameObject parentisation;
+	public bool forestsizing = false;
+	public bool grasssizing = false;
 
 	// Use this for initialization
 	void Start () {
 		parentisation = GameObject.Find ("HexFolder");
 		this.transform.parent = parentisation.transform; // Cleans up nicely. :P
-	
+	if (forestsizing)
+		{ 
+			this.transform.localScale += new Vector3(0,0, Random.Range(-5, 10));
+		}
+		if (grasssizing)
+		{ 
+			this.transform.localScale += new Vector3(0,0, Random.Range(-2, 1));
+		}
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class HexGrid : MonoBehaviour {
 	public Transform spawnThis;   
 	
-	public int x = 5;
-	public int z = 5;
+	public static int mapWidth = 25;
+	public static int mapLength = 25;
 
-	public int mapWidth = 25;
-	public int mapLength = 25;
+	private static float x = 25;
+	private static float z = 25;
 
+	private int innx, innz, dunx, dunz, reroll, savex, savez, davex, davez;
 	public float radius = 0.5f;
 	public bool useAsInnerCircleRadius = true;
 	
@@ -19,16 +20,17 @@ public class HexGrid : MonoBehaviour {
 	
 	//Inn, road, grass, forest, dungeon
 
-	private int[][] mapMaking = 
-	{
-		new int[]{ 3, 3, 3, 3, 3, 3, 3 },
-		new int[]{ 3, 0, 2, 2, 2, 2, 3 },
-		new int[]{ 3, 2, 1, 1, 2, 2, 3 },
-		new int[]{ 3, 2, 2, 2, 1, 2, 3 }
-		new int[]{ 3, 2, 2, 2, 2, 4, 3 },
-		new int[]{ 3, 3, 3, 3, 3, 3, 3 },
-		
-	};
+	private int[,] mapMaking = new int[mapWidth, mapLength];
+//	{
+//		new int[]{ 3, 3, 3, 3, 3, 3, 3, 3 },
+//		new int[]{ 3, 0, 2, 2, 2, 2, 2, 3 },
+//		new int[]{ 3, 2, 1, 1, 2, 2, 2, 3 },
+//		new int[]{ 3, 2, 2, 2, 1, 1, 2, 3 },
+//		new int[]{ 3, 2, 2, 2, 2, 2, 4, 3 },
+//		new int[]{ 3, 3, 3, 3, 3, 3, 3, 3 },
+//	};
+	private int tileType;
+
 
 	void Start() {
 		float unitLength = ( useAsInnerCircleRadius )? (radius / (Mathf.Sqrt(3)/2)) : radius;
@@ -37,13 +39,71 @@ public class HexGrid : MonoBehaviour {
 		offsetZ = unitLength * 1.5f;
 		
 
-		for( int i = 0; i < 6; i++ ) {
-			for( int j = 0; j < 7; j++ ) {
-				Vector3 hexpos = HexOffset( i, j );
-				Vector3 pos = new Vector3( hexpos.x, 0, hexpos.z);
-				Instantiate(tile[ mapMaking[i][j] ], pos, transform.rotation );
+		for( int recount = 0; recount < 2; recount++) {
+			for( int i = 0; i < x; i++ ) {
+				for( int j = 0; j < z; j++ ) {
+			
+					tileType = 2;
+					if (i < 2 || i > (x - 2) || j < 2 || j > (z - 2)) {tileType = 3;}; // borders are forest
+					if (i > 1 && i < x-1 && j > 1 && j < z-1) 
+					{
+						if (i < x*.4 && Random.Range(0, 10) < 3 && (mapMaking[i-1,j] == 3)) {tileType = 3;};
+						if (i < x*.4 && Random.Range(0, 10) < 3 && (mapMaking[i-1,j+1] == 3)) {tileType = 3;};
+						if (i > (x - x*.4) && Random.Range(0, 10) < 3 && (mapMaking[i+1,j] == 3)) {tileType = 3;};
+						if (i > (x - x*.4) && Random.Range(0, 10) < 3 && (mapMaking[i+1,j+1] == 3)) {tileType = 3;};
+						if (j < z*.4 && Random.Range(0, 10) < 3 && (mapMaking[i+1,j-1] == 3)) {tileType = 3;};
+						if (j < z*.4 && Random.Range(0, 10) < 3 && (mapMaking[i-1,j-1] == 3)) {tileType = 3;};
+						if (j > (z - z*.4) && Random.Range(0, 10) < 3 && (mapMaking[i+1,j+1] == 3)) {tileType = 3;};
+						if (j > (z - z*.4) && Random.Range(0, 10) < 3 && (mapMaking[i-1,j+1] == 3)) {tileType = 3;};
+					}
+					mapMaking[i,j] = tileType;
+				}
 			}
 		}
+		innx = Random.Range (4, 9); 
+		innz = Random.Range (4, 9); 
+		dunx = Random.Range (17, 23); 
+		dunz = Random.Range (17, 23);
+
+		savex = innx;
+		savez = innz;
+		davex = dunx;
+		davez = dunz;
+
+		mapMaking[dunx, dunz] = 4;
+		mapMaking[innx, innz] = 0;
+
+		for( int i = 0; i < x*2; i++){
+			reroll = Random.Range (0, 80);
+			if (dunx > innx || dunz > innz)
+			{
+				if (reroll < 40 && dunx != innx) 
+				{
+					innx++;
+					mapMaking[innx, innz] = 1;
+
+				}
+				if (reroll > 39 && dunz != innz) 
+				{
+					innz++;
+					mapMaking[innx, innz] = 1;
+				}
+			}
+			if (dunz == innz && innx < dunx) {innx++; mapMaking[innx, innz] = 1;}
+			if (dunx == innx && innz < dunz) {innz++; mapMaking[innx, innz] = 1;}
+		}
+		mapMaking[savex, savez] = 0;
+		mapMaking[davex, davez] = 4;
+		for( int i = 0; i < x; i++ ) {
+			for( int j = 0; j < z; j++ ) {
+			
+				
+				Vector3 hexpos = HexOffset( i, j );
+				Vector3 pos = new Vector3( hexpos.x, 0, hexpos.z);
+				Instantiate(tile[ mapMaking[i,j] ], pos, transform.rotation );
+			}
+		}
+
 	}
 	
 	Vector3 HexOffset( int x, int z ) {
